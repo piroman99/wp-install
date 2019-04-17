db1725a21e69e23e4bf239c4b084250ff27b3361
Ivan Karpoukhin <piroman99@yandex.ru>
2019-04-17 22:52:19 +0300

    Update install-wp.sh

--- install-wp.sh
+++ install-wp.sh
@@ -3,6 +3,6 @@ apt update -y #ну надо
 apt upgrade -y #очень надо
 apt install -y zram-config #Я художник, я так вижу
 apt install -y tasksel # Потому что я ленивая #опа
-tasksel install -y lamp-server# Все упрощаем
+yes |tasksel install lamp-server# Все упрощаем
 apt install -y nginx #Куда же без него
 
