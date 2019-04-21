9af5e7303b824aebfc47691a30b970611aaeeffe
Ivan Karpoukhin <piroman99@yandex.ru>
2019-04-21 21:03:02 +0300

    Update install-wp.sh

--- install-wp.sh
+++ install-wp.sh
@@ -1,7 +1,8 @@
  #!/bin/bash
 
 
- #А тут надо узнать домен, e-mail админа, и может быть пароль админа
+ #А тут надо узнать домен, e-mail админа, и может быть пароль админа из файла или консоли прочитать.
+ 
  
 # и в Продакшен
 apt update -y #ну надо
@@ -12,9 +13,11 @@ apt install -y lamp-server^ # Все упрощаем
 #mysql_secure_installation - я не знаю как автоматом без ввода root пароля
 #apt install -y nginx #Куда же без него - но связку еще надо настроить
 #вот тут настроили связки nginx-apache
+#А тут подкрутили все под максимум производительности
+
+#Включаем файрвол
 ufw allow ssh # http https  на самом деле нужно добавить несколько правил - но как. по одному что ли
 #ufw enable
-
 
 #Тут мы еще решим вопрос с DNS и SSL
 
@@ -23,8 +26,24 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 sudo mv wp-cli.phar /usr/local/bin/wp
 
+#cd /var/www/domain/
+#Загружаем wordpress c заданной локалью
+#wp core download --locale=$locale --allow-root
+
+#Создаем конфиг wordpress
+#wp core config --dbname=wordpress --dbuser=wordpress --dbpass=$password --dbhost=localhost --dbprefix=wp_ --allow-root
+
+#устанавливаем wordpress
+#wp core install --url="вашдомен.ru"  --title="My blog" --admin_user="admin" --admin_password="пароль_администратора" —admin_email="мойemail@email.ru" --allow-root
+
+#Активируем supercache
+#wp plugin install wp-super-cache --allow-root
+#wp plugin activate wp-super-cache --allow-root
+#wp super-cache enable  --allow-root
+
 
 
 #Закончили
+rm -f wphosters.cfg
 rm -f install-wp.sh #Убираем следы нашего позора
 shutdown -r now #Семь бед - один ресет
